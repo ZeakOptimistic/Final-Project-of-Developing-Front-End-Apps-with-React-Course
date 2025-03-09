81b2f5caf1025d23dbff7346f1f20677d563e37d
Zeak Optimistic <quangkhaint2005@gmail.com>
2025-03-09 18:44:55 +0700

    hopefully it can run

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/Final-Project-of-Developing-Front-End-Apps-with-React-Course/",
   plugins: [react()],
 })
